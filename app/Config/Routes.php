3c618b545898cd8c8e0e28bd1d8c33fbe5b67842
joancorderroure <joan.corderroure@gmail.com>
2022-05-03 16:05:25 +0200

    Added the necessary lines for the config of the Myth/Auth

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,6 +34,35 @@ $routes->setAutoRoute(false);
 $routes->get('/', 'Home::index');
 
 /*
+*-----------------------------------------------------------------------
+*   MYTH AUTH SERVICE ROUTES
+*-----------------------------------------------------------------------
+*/
+
+// Login/out
+$routes->get('login', 'AuthController::login', ['as' => 'login']);
+$routes->post('login', 'AuthController::attemptLogin');
+$routes->get('logout', 'AuthController::logout');
+
+// Registration
+$routes->get('register', 'AuthController::register', ['as' => 'register']);
+$routes->post('register', 'AuthController::attemptRegister');
+
+// Activation
+$routes->get('activate-account', 'AuthController::activateAccount', ['as' => 'activate-account']);
+$routes->get('resend-activate-account', 'AuthController::resendActivateAccount', ['as' => 'resend-activate-account']);
+
+// Forgot/Resets
+$routes->get('forgot', 'AuthController::forgotPassword', ['as' => 'forgot']);
+$routes->post('forgot', 'AuthController::attemptForgot');
+$routes->get('reset-password', 'AuthController::resetPassword', ['as' => 'reset-password']);
+$routes->post('reset-password', 'AuthController::attemptReset');
+
+
+
+
+
+/*
  * --------------------------------------------------------------------
  * Additional Routing
  * --------------------------------------------------------------------
