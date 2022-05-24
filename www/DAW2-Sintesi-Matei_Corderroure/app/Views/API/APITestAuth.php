296b6af8d39e0b4c34bbd54cc61994709abc3767
joancorderroure <joan.corderroure@gmail.com>
2022-05-24 16:25:52 +0200

    Token API change

--- www/DAW2-Sintesi-Matei_Corderroure/app/Views/API/APITestAuth.php
+++ www/DAW2-Sintesi-Matei_Corderroure/app/Views/API/APITestAuth.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <style>
-        #token {
+        #tokenLog {
             word-wrap: break-word;
         }
     </style>
@@ -151,17 +151,20 @@
             }),
         });
         response.json().then((data) => {
-            if (data.error = false) {
+            if (data.error == false) {
                 console.log(data.token);
                 console.log(data.messages);
                 console.log(data.status);
 
+                document.getElementById("errorsLog").innerHTML = "";
                 document.getElementById("tokenLog").innerHTML = "Token: " + data.token;
             } else {
                 console.log(data.token);
                 console.log(data.messages);
                 console.log(data.status);
+
                 document.getElementById("errorsLog").innerHTML = "Errors: " + data.messages;
+                document.getElementById("tokenLog").innerHTML = "";
             }
         });
     }
